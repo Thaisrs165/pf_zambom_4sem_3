40d351aea723becd1faa31e292a77ac5522388c3
Thaisrs165 <thaisrs3@al.insper.edu.br>
2025-11-10 10:14:59 -0300

    aplicacao 2

--- src/main/java/br/edu/insper/pf/filmes/FilmeRepository.java
+++ src/main/java/br/edu/insper/pf/filmes/FilmeRepository.java
@@ -1,7 +1,7 @@
 package br.edu.insper.pf.filmes;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-public class FilmeRepository extends JpaRepository<Filme, Integer> {
+public interface FilmeRepository extends MongoRepository<Filme, Integer> {
 }
